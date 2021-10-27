1f61e6f8ccb0d0cdc4965a99d3e33599b732f8ef
Aska Erlangga <ae.askaerlangga@gmail.com>
2021-10-27 20:11:17 +0700

    Field

--- bin/class.dart
+++ bin/class.dart
@@ -1,10 +1,21 @@
 class Person{
-
+  //--- Field ---
+  //nama = wajib diisi
+  String nama = "Guest";
+  //alamat = Nullable
+  String? alamat;
+  //negara = bersifat final, tidak bisa diubah
+  final String negara = "Indonesia";
 }
 
 void main(){
   var person1 = Person();
-  print(person1);
+  //Mengubah data Field
+  person1.nama = "Aska Erlangga";
+  person1.alamat = "Kota Serang";
+  print(person1.nama);
+  print(person1.alamat);
+  print(person1.negara);
 
   var person2 = Person();
   print(person2);

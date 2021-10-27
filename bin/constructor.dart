4ca0391080f06b7b639fec373db40ea3ae0a9788
Aska Erlangga <ae.askaerlangga@gmail.com>
2021-10-27 21:47:50 +0700

    Named Constructor

--- bin/constructor.dart
+++ bin/constructor.dart
@@ -1,15 +1,27 @@
 class Person{
   String nama = "Guest";
-  String? alamat;
+  String? kota;
   final String negara = "Indonesia";
 
   //Constructor = Method yang dipanggil saat membuat object
   //Formal Parameter
-  Person(this.nama, this.alamat);
+  Person(this.nama, this.kota);
+
+  //Named Constructor, membuat lebih dari satu constructor
+  Person.denganNama(this.nama);
+  Person.denganKota(this.kota);
 }
 
 void main(){
   var person1 = Person("Aska Erlangga", "Kota Serang");
   print(person1.nama);
-  print(person1.alamat);
+  print(person1.kota);
+
+  var person2 = Person.denganNama("Edo");
+  print(person2.nama);
+  print(person2.kota);
+
+  var person3 = Person.denganKota("Jakarta");
+  print(person3.nama);
+  print(person3.kota);
 }

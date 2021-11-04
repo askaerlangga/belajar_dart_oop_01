749e555ca034ccb9fdad5d5214fd2add6e828c6e
Aska Erlangga <ae.askaerlangga@gmail.com>
2021-11-04 22:16:37 +0700

    Equals Operator

--- bin/data/category.dart
+++ bin/data/category.dart
@@ -3,4 +3,19 @@ class Category{
   String nama;
 
   Category(this.id, this.nama);
+
+  bool operator ==(Object other){
+    if(other is Category){
+      if(id != other.id){
+        return false;
+      }else if(nama != other.nama){
+        return false;
+      }else{
+        return true;
+      }
+    }else{
+      return false;
+    }
+    
+  }
 }

3d56dea0fd451073e2f7a1499cef382322a75038
Aska Erlangga <ae.askaerlangga@gmail.com>
2021-11-04 22:48:09 +0700

    Callable Class

---
+++ bin/callable_class.dart
@@ -0,0 +1,7 @@
+import 'data/sum.dart';
+
+void main(){
+  var sum = Sum(20, 20);
+
+  print(sum());
+}
---
+++ bin/data/sum.dart
@@ -0,0 +1,10 @@
+class Sum{
+  int angka1;
+  int angka2;
+
+  Sum(this.angka1,this.angka2);
+
+  int call(){
+    return angka1 + angka2;
+  }
+}

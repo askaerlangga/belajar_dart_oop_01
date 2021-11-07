f6b10276db296654833e88d0d97b4afbb465585c
Aska Erlangga <ae.askaerlangga@gmail.com>
2021-11-07 14:58:38 +0700

    Static field

---
+++ bin/static_field.dart
@@ -0,0 +1,11 @@
+class Applications{
+  //Static field
+  static final String nama = "Aplikasiku";
+  static final String author = "Aska Erlangga";
+}
+
+void main(){
+  //Tidak perlu membuat object, langsung panggil nama class nya
+  print(Applications.nama);
+  print(Applications.author);
+}

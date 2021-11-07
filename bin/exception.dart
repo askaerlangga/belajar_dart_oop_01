083774c534b6906fa44eebb205b3fb0c4a279219
Aska Erlangga <ae.askaerlangga@gmail.com>
2021-11-07 20:35:02 +0700

    Exception

---
+++ bin/exception.dart
@@ -0,0 +1,19 @@
+class ValidationException implements Exception{
+  String message;
+  ValidationException(this.message);
+}
+
+class Validation{
+  static void validate(String username, String password){
+    if(username == ""){
+      throw ValidationException("Username kosong!");
+    }else if(password == ""){
+      throw ValidationException("Password kosong!");
+    }
+
+  }
+}
+
+void main(){
+  Validation.validate("", "Hello");
+}
